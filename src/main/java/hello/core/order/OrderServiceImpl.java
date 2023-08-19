6c035047d6dce78a5a7e68068dc1835a517e2d26
kiarakim <kiarakim413@gmail.com>
2023-08-19 21:30:37 +0900

    feat: 의존관계 자동 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,27 +1,29 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-	private final MemberRepository memberRepository;
-	private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-	@Autowired
-	public OrderServiceImpl(final MemberRepository memberRepository, final DiscountPolicy discountPolicy) {
-		this.memberRepository = memberRepository;
-		this.discountPolicy = discountPolicy;
-	}
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
-	@Override
-	public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
-		final Member member = memberRepository.findById(memberId);
-		final int discountPrice = discountPolicy.discount(member, itemPrice);
+    @Override
+    public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
+        final Member member = memberRepository.findById(memberId);
+        final int discountPrice = discountPolicy.discount(member, itemPrice);
 
-		return new Order(memberId, itemName, itemPrice, discountPrice);
-	}
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
 }
